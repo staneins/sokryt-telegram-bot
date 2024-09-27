353d0d2e245d224997f86610fe15155195038efb
Philip Kaminsky <kaminsky8836@gmail.com>
2024-09-27 16:19:32 +0300

    implement connection to the database, repo, user classes

---
+++ src/main/java/com/kaminsky/model/User.java
@@ -0,0 +1,71 @@
+package com.kaminsky.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity(name = "usersDataTable")
+public class User {
+
+    @Id
+    private Long chatId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String userName;
+
+    private Timestamp registeredAt;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "chatId=" + chatId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", registeredAt=" + registeredAt +
+                '}';
+    }
+
+    public Timestamp getRegisteredAt() {
+        return registeredAt;
+    }
+
+    public void setRegisteredAt(Timestamp registeredAt) {
+        this.registeredAt = registeredAt;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+}
